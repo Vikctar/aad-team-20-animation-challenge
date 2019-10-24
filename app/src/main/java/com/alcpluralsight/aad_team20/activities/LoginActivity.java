e726af2327d37681899a17d9b7df4580d3b8b6a6
TheOnlySmartBoy <o2jose43@gmail.com>
2019-10-24 12:33:56 +0300

    Begun adding login code

---
+++ app/src/main/java/com/alcpluralsight/aad_team20/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.alcpluralsight.aad_team20.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alcpluralsight.aad_team20.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
